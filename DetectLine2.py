f4a38fe71d8e13f69f7dc1af15621f41ed7ba664
domika1 <domika1@naver.com>
2023-03-31 11:27:08 +0900

    차선 표기선 굵기 수정

--- DetectLine2.py
+++ DetectLine2.py
@@ -1,5 +1,29 @@
 import cv2
 import numpy as np
+import os, sys
+import serial
+import time
+
+# 시리얼 장치 연결
+# 보레이드 9600, 장치 위치 ttyUSB0
+ser = serial.Serial('/dev/ttyUSB0',9600, timeout = 5)
+
+def run():
+    #시리얼 쓰기
+    ser.write(bytes(bytearray([0x01,0x02,0x03,0x04,0x05])))
+    while True:
+        # 시리얼 읽기 (5바이트씩 읽음)
+        line = ser.read(5)
+        if len(line) == 0:
+                break;
+        # 헥사 코드로 출력
+        hex_list = ["{:x}".format(ord(c)) for c in line];
+        print ''.join(hex_list)
+while True:
+    run();
+
+
+
 def DetectBlueLine(src):
 
     #HSV로 변환후 파란색만 추출
@@ -86,7 +110,7 @@ while cam.isOpened():
                     print('right')
             elif abs(l - 15) > abs(r):
                 print('right')
-            elif abs(r + 15) > abs(l):q
+            elif abs(r + 15) > abs(l):
                 print('left')
             else:
                 print('go')
